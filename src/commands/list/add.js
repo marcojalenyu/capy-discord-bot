5f60a6fca84d4e4779e0c002cf7572304a51bc79
Maracoo <marco_jalen_yu@dlsu.edu.ph>
2024-05-29 13:46:30 +0800

    Added /add command

---
+++ src/commands/list/add.js
@@ -0,0 +1,110 @@
+const { ApplicationCommandOptionType } = require("discord.js");
+const List = require("../../models/List");
+const Reminder = require("../../models/Reminder");
+
+module.exports = {
+    name: 'add',
+    description: 'Add a reminder to the list.',
+    options: [
+        {
+            name: 'category',
+            description: 'English, Math, Cooking, etc.',
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+        {
+            name: 'name',
+            description: 'Quiz 1, Help Mom, etc.',
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+        {
+            name: 'deadline',
+            description: 'Must follow MM/DD/YYYY format.',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        },
+        {
+            name: 'time',
+            description: 'Must follow HH:MM format.',
+            type: ApplicationCommandOptionType.String,
+            required: false
+        },
+        {
+            name: 'description',
+            description: 'The description of the reminder.',
+            type: ApplicationCommandOptionType.String,
+            required: false
+        },
+        {
+            name: 'type',
+            description: 'The type of reminder.',
+            type: ApplicationCommandOptionType.String,
+            required: false,
+            choices: [
+                {
+                    name: 'Task',
+                    value: 'task'
+                },
+                {
+                    name: 'Event',
+                    value: 'event'
+                }
+            ]
+        }
+    ],
+
+    callback: async (client, interaction) => {
+        try {   
+            // Find the list associated with the guildId or userId
+            const list = await List.findOne({ guildId: interaction.guildId }) || await List.findOne({ userId: interaction.user.id });
+            // If the list does not exist, reply with an error message and return
+            if (!list) {
+                interaction.reply({ 
+                    content: "You do not have a list registered.",
+                    ephemeral: true
+                });
+                return;
+            } else {
+                // Get the reminder details from the interaction
+                const category = interaction.options.getString('category');
+                const name = interaction.options.getString('name');
+                const deadline = interaction.options.getString('deadline');
+                const time = interaction.options.getString('time');
+                const description = interaction.options.getString('description');
+                const type = interaction.options.getString('type');
+
+                // Create a new reminder
+                const newReminder = new Reminder({
+                    listId: list._id,
+                    category: category,
+                    name: name,
+                    deadline: new Date(deadline),
+                    description: description || '',
+                    type: type || 'task'
+                });
+
+                // If the reminder has a time, set the time
+                if (time) {
+                    const [hours, minutes] = time.split(':');
+                    newReminder.deadline.setUTCHours(hours);
+                    newReminder.deadline.setUTCMinutes(minutes);
+                    newReminder.deadline.setUTCSeconds(59);
+                } else {
+                    newReminder.deadline.setUTCHours(23);
+                    newReminder.deadline.setUTCMinutes(59);
+                    newReminder.deadline.setUTCSeconds(59);
+                }
+
+                // Save the reminder in the database
+                await newReminder.save();
+                list.reminders.push(newReminder);
+                await list.save();
+
+                interaction.reply({ content: `Reminder "${category} - ${name}" added.` });
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
