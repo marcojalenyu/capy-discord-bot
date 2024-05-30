1999c570d656a8fdb6dcf280490e6bfd182ebebb
Maracoo <marco_jalen_yu@dlsu.edu.ph>
2024-05-30 14:27:26 +0800

    Added a loop to send list if at the designated time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
+const List = require('./models/List');
 const mongoose = require('mongoose');
 const eventHandler = require('./handlers/eventHandler');
+const sendReminders = require('./utils/sendReminders');
 
 const client = new Client({ 
     intents: [ 
@@ -12,11 +14,25 @@ const client = new Client({
     ]
 });
 
-(() => {
+(async () => {
     try {
         mongoose.connect(process.env.MONGODB_URI);
         console.log('Connected to MongoDB');
         eventHandler(client);
+        
+        // Checks if it is time to remind the user (every minute)
+        setInterval(async () => {
+            const now = new Date();
+            const currentTime = `${now.getHours()}:${now.getMinutes()}`;
+            console.log(currentTime);
+
+            const lists = await List.find({ remindTime: currentTime });
+
+            for (const list of lists) {
+                sendReminders(client, list);
+            }
+            
+        }, 60 * 1000);
     
     } catch (error) {
         console.error('Error connecting to MongoDB');
